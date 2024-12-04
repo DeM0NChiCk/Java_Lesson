c6d87702677a06533d90a44daff49c0556fe3d97
Gulnaz-29 <gulnaz.nasib@gmail.com>
2024-12-04 18:09:03 +0300

    Добавила 3 метода в классе Storage: 1. addNewStorage() - в параметрах имеет исходный массив, а также массив, созданный в результате расширения склада 2. printArray() - выводит список позиций наименований товаров с конечной информацией о количестве свободных позиций (мест) на складе 3. addNewPosition() - добавляет новую/новые позиции (наименования)  товара

--- ShopOOP/src/Main.java
+++ ShopOOP/src/Main.java
@@ -1,17 +1,17 @@
 import model.Product;
 import model.Storage;
 
+import java.util.Scanner;
 import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Привет магазин!");
         Storage storage = new Storage();
 
         Product[] productsOne = new Product[3];
-        System.out.println(Arrays.toString(productsOne));
+//      System.out.println(Arrays.toString(productsOne));
 
-        productsOne[0] = new Product("table", 0, 5);
+        productsOne[0] = new Product("table", 1, 5);
         productsOne[1] = new Product("chair", 1, 10);
         productsOne[2] = new Product("bookshelf", 2, 3);
 
@@ -33,15 +33,27 @@ public class Main {
         storage.addProducts(productsThree, chair);
         storage.addProducts(productsThree, bookshelf);
 
-        System.out.println(Arrays.toString(productsOne));
-        System.out.println(Arrays.toString(productsTwo));
-        System.out.println(Arrays.toString(productsThree));
+//      System.out.println(Arrays.toString(productsOne));
+//      System.out.println(Arrays.toString(productsTwo));
+//      System.out.println(Arrays.toString(productsThree));
 
-        storage.getProducts(productsThree);
+//      storage.getProducts(productsThree);
         storage.buyProducts(productsThree, table);
-
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите число, на которое расширяется склад: ");
+// расширение склада---------------------------------------------------------------------------
+        int inputPlusNumber = scan.nextInt();
+        Product[] productsOneNew = new Product[productsOne.length+inputPlusNumber];
+        storage.addNewStorage(productsOne, productsOneNew, inputPlusNumber);
+//      productsOneNew[3] = new Product("cupboard", 3, 2);
+//      productsOneNew[4] = new Product("sofa", 3, 1);
+        storage.printArray(productsOneNew);
+        storage.addNewPositoin(productsOneNew);
+        storage.printArray(productsOneNew);
+//----------------------------------------------------------------------------------------------
         // TODO: подумать над реализацией static методов
 
     }
+
 
 }
