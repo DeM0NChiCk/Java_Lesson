f1b9df6e965444340d9c65d2ee9fb30f93488ee6
Mexcal <Mexcalli@proton.me>
2025-01-17 15:47:20 +0300

    Изменения:
    1) Интерфейс Storage переписан с дженериками.
    
    2) Моя реализация класса Storage исправлена для возможности использования нового Storage. Дженерик T расширяет Produсt, иначе код не компилируется (не видит методы).
    
    3) Все остальные имплементации Storage закомментированы, чтобы Main смог запуститься.

--- ShopOOP/src/service/impl/StorageNewImpl.java
+++ ShopOOP/src/service/impl/StorageNewImpl.java
@@ -1,42 +1,42 @@
-package service.impl;
-
-import enums.EnumStatusCode;
-import model.Product;
-import service.StorageNew;
-
-public class StorageNewImpl implements StorageNew {
-    @Override
-    public void addProduct(Product[] products, Product product) {
-
-    }
-
-    @Override
-    public void getAllProducts(Product[] products) {
-        getAllProducts();
-    }
-
-    @Override
-    public EnumStatusCode buyProducts(Product[] products, Product product) {
-        return EnumStatusCode.SUCCESSFULLY;
-    }
-
-    @Override
-    public Product[] storageExpansion(Product[] products, int extensionValue) {
-        return new Product[0];
-    }
-
-    @Override
-    public void deliveryProducts(Product[] storage, Product[] deliveryProducts) {
-
-    }
-
-    @Override
-    public void getAllProducts() {
-
-    }
-
-    @Override
-    public void log() {
-
-    }
-}
+//package service.impl;
+//
+//import enums.EnumStatusCode;
+//import model.Product;
+//import service.StorageNew;
+//
+//public class StorageNewImpl implements StorageNew {
+//    @Override
+//    public void addProduct(Product[] products, Product product) {
+//
+//    }
+//
+//    @Override
+//    public void getAllProducts(Product[] products) {
+//        getAllProducts();
+//    }
+//
+//    @Override
+//    public EnumStatusCode buyProducts(Product[] products, Product product) {
+//        return EnumStatusCode.SUCCESSFULLY;
+//    }
+//
+//    @Override
+//    public Product[] storageExpansion(Product[] products, int extensionValue) {
+//        return new Product[0];
+//    }
+//
+//    @Override
+//    public void deliveryProducts(Product[] storage, Product[] deliveryProducts) {
+//
+//    }
+//
+//    @Override
+//    public void getAllProducts() {
+//
+//    }
+//
+//    @Override
+//    public void log() {
+//
+//    }
+//}
